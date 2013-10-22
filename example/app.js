fbb6fb3a84412b6fbd7b4d69106ba1031e88b221
Corey Burrows <corey.burrows@centro.net>
2013-10-21 21:11:24 -0500

    Updates example app based on updated statechart api.

--- example/app.js
+++ example/app.js
@@ -20,88 +20,88 @@ angular.module('app', ['state', 'router'])
     $statechart.state('start');
 
     $statechart.state('index', function() {
-      this.enter = function() {
+      this.enter(function() {
         $router.path('/campaigns').search({});
         $state.contentTmpl = 'index.html';
-      };
+      });
 
-      this.didSelectCampaign = function(id) {
+      this.action('didSelectCampaign', function(id) {
         this.goto('/show', {context: id});
-      };
+      });
     });
 
     $statechart.state('show', {isConcurrent: true}, function() {
-      this.enter = function(id) {
+      this.enter(function(id) {
         $router.path('/campaigns/' + id).search({});
         $state.contentTmpl      = 'show.html';
         $state.selectedCampaign = campaigns[id];
-      };
+      });
 
-      this.exit = function() { delete $state.selectedCampaign; };
+      this.exit(function() { delete $state.selectedCampaign; });
 
       this.state('filter', function() {
         angular.forEach(['all', 'live', 'issues', 'completed'], function(name) {
           this.state(name, function() {
-            this.enter = function() {
+            this.enter(function() {
               $router.search().filter = $state.selectedCampaignFilter = name;
-            }
+            });
           });
         }, this);
 
-        this.didSelectFilter = function(filter) {
+        this.action('didSelectFilter', function(filter) {
           $router.replace(); this.goto(filter);
-        };
+        });
       });
 
       this.state('metric', function() {
         angular.forEach(['conversions', 'pacing', 'delivery'], function(name) {
           this.state(name, function() {
-            this.enter = function() {
+            this.enter(function() {
               $router.search().metric = $state.selectedCampaignMetric = name;
-            };
+            });
           });
         }, this);
 
-        this.didSelectMetric = function(metric) {
+        this.action('didSelectMetric', function(metric) {
           $router.replace(); this.goto(metric);
-        };
+        });
       });
 
       this.state('modal', function() {
         this.state('off', function() {
-          this.enter = function() {
+          this.enter(function() {
             delete $router.search().modal;
             $state.showCampaignModal = false;
-          };
+          });
 
-          this.didToggleModal = function() {
+          this.action('didToggleModal', function() {
             $router.replace(); this.goto('../on');
-          };
+          });
         });
 
         this.state('on', function() {
-          this.enter = function() {
+          this.enter(function() {
             $router.search().modal = true;
             $state.showCampaignModal = true;
-          };
+          });
 
-          this.didToggleModal = function() {
+          this.action('didToggleModal', function() {
             $router.replace(); this.goto('../off');
-          };
+          });
         });
       });
     });
 
     $statechart.state('unknownRoute', function() {
-      this.enter = function(url) {
+      this.enter(function(url) {
         $state.contentTmpl = 'unknown_route.html';
         $state.unknownUrl = url;
-      };
+      });
 
-      this.exit = function() { delete $state.unknownUrl; };
+      this.exit(function() { delete $state.unknownUrl; });
     });
 
-    $statechart.didRouteTo = function(route, params, search) {
+    $statechart.action('didRouteTo', function(route, params, search) {
       var id, filter, metric, modal;
 
       switch (route) {
@@ -121,11 +121,11 @@ angular.module('app', ['state', 'router'])
                     {context: id, force: true});
           break;
       }
-    };
+    });
 
-    $statechart.didRouteToUnknown = function(url) {
+    $statechart.action('didRouteToUnknown', function(url) {
       this.goto('/unknownRoute', {context: url});
-    };
+    });
 
     $statechart.goto();
   })
